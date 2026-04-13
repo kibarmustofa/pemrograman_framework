8c74137b85df2cf903c02c43ba28b30be85d1e2e
kibarmustofa <kibarmustofa115@gmail.com>
2026-04-13 21:13:05 +0700

    16.JOBSHEET PRAKTIKUM-Implementasi Login Database & Multi-Role - BAGIAN 6 dan tugas

--- src/middleware/withAuth.ts
+++ src/middleware/withAuth.ts
@@ -1,6 +1,8 @@
 import { getToken } from "next-auth/jwt";
 import { NextFetchEvent, NextMiddleware, NextRequest, NextResponse } from "next/server";
 
+const hanyaAdmin = ["/admin"];
+
 export default function withAuth(
   middleware: NextMiddleware,
   requireAuth: string[] = [],
@@ -19,7 +21,12 @@ export default function withAuth(
         url.searchParams.set("callbackUrl", encodeURI(req.url));
         return NextResponse.redirect(url);
       }
+
+      if (token.role !== "admin" && hanyaAdmin.includes(pathname)) {
+        return NextResponse.redirect(new URL("/", req.url));
+      }
     }
+
     return middleware(req, next);
-  }
+  };
 }
