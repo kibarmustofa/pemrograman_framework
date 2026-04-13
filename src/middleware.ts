9b6892dad35d3af0ff11c6fe8dae6002492d7584
kibarmustofa <kibarmustofa115@gmail.com>
2026-04-13 21:12:48 +0700

    16.JOBSHEET PRAKTIKUM-Implementasi Login Database & Multi-Role - BAGIAN 6 dan tugas

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,28 @@
 import { NextResponse } from "next/server";
 import type { NextRequest } from "next/server";
 import { getToken } from "next-auth/jwt";
+import withAuth from "./middleware/withAuth"
 
-export async function middleware(request: NextRequest) {
-  const token = await getToken({
-    req: request,
-    secret: process.env.NEXTAUTH_SECRET,
-  });
+// export async function middleware(request: NextRequest) {
+//   const token = await getToken({
+//     req: request,
+//     secret: process.env.NEXTAUTH_SECRET,
+//   });
 
-  if (token) {
-    return NextResponse.next();
-  }
+//   if (token) {
+//     return NextResponse.next();
+//   }
 
-  return NextResponse.redirect(new URL("/", request.url));
+//   return NextResponse.redirect(new URL("/", request.url));
+// }
+export function mainMiddleware(request: NextRequest) {
+  const res = NextResponse.next();
+  return res;
 }
 
+// Bagian ini yang krusial: export default withAuth
+export default withAuth(mainMiddleware, ["/produk", "/about", "/profile", "/admin"]);
+
 export const config = {
-  matcher: ["/produk", "/about", "/profile"],
+  matcher: ["/produk", "/about", "/profile", "/admin"],
 };
