69b12a22bdfe07f6b144c1f7c36ed7a77d8327e9
kibarmustofa <kibarmustofa115@gmail.com>
2026-04-14 08:52:43 +0700

    17.JOBSHEET PRAKTIKUM-Implementasi Login Google Provider dengan NextAuth.js + Firebase [D. Konfigurasi Google Provider di NextAuth dan Handle Callback JWT & Session]

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,19 @@
 import NextAuth, { NextAuthOptions } from "next-auth";
 import CredentialsProvider from "next-auth/providers/credentials";
+import GoogleProvider from "next-auth/providers/google"; 
 import { signIn } from "@/utils/db/serviceFirebase";
 import bcrypt from "bcrypt";
 
 export const authOptions: NextAuthOptions = {
   session: {
-    strategy: "jwt",
+    strategy: "jwt", 
   },
   secret: process.env.NEXTAUTH_SECRET,
   providers: [
+    GoogleProvider({ 
+      clientId: process.env.GOOGLE_CLIENT_ID || "", 
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || "", 
+    }),
     CredentialsProvider({
       name: "credentials",
       credentials: {
@@ -18,18 +23,15 @@ export const authOptions: NextAuthOptions = {
       async authorize(credentials) {
         if (!credentials?.email || !credentials?.password) return null;
 
-        // Mencari user di Firebase berdasarkan email
         const user: any = await signIn(credentials.email);
 
         if (user) {
-          // Membandingkan password input dengan hash di database
           const isPasswordValid = await bcrypt.compare(
             credentials.password,
             user.password
           );
 
           if (isPasswordValid) {
-            // Mengembalikan data user untuk disimpan di token
             return {
               id: user.id,
               email: user.email,
@@ -43,26 +45,54 @@ export const authOptions: NextAuthOptions = {
     }),
   ],
 
-  callbacks: {
+ callbacks: {
     async jwt({ token, account, user }: any) {
       if (account?.provider === "credentials" && user) {
         token.email = user.email;
         token.fullname = user.fullname;
         token.role = user.role;
       }
+
+      if (account?.provider === "google") {
+        const data = {
+          fullname: user.name,
+          email: user.email,
+          image: user.image,
+          type: account.provider,
+        };
+        
+        token.fullname = data.fullname;
+        token.email = data.email;
+        token.image = data.image;
+        token.type = data.type;
+      }
+      
       return token;
     },
+
     async session({ session, token }: any) {
       if (token.email) {
-        session.user.email = token.email;
+        session.user.email = token.email; 
       }
       if (token.fullname) {
         session.user.fullname = token.fullname;
       }
-      if (token.role) {
-        session.user.role = token.role;
+      
+      
+      if (token.image) {
+        session.user.image = token.image;
       }
-      return session;
+      
+      if (token.role) {
+        session.user.role = token.role; 
+      }
+      
+      
+      if (token.type) {
+        session.user.type = token.type;
+      }
+
+      return session; 
     },
   },
 
